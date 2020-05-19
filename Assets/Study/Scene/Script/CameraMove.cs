32c47196e9f355996da8795c4009ae1e2112c0d8
minho <chaminho88@naver.com>
2020-05-20 00:25:56 +0900

    Make Fist Unity Project Scene

---
+++ Assets/Study/Scene/Script/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+    public Transform target;
+    private Transform tr;
+
+	// Use this for initialization
+	void Start () {
+
+        tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        tr.position = new Vector3(target.position.x, target.position.y + 20.0f, target.position.z);
+        tr.LookAt(target);
+	}
+}
---
+++ Assets/Study/Scene/Script/PlayerCtrl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+
+    public int Speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Speed * Time.deltaTime);
+        }
+    }
+}

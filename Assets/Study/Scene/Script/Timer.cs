aa074636035132b3159c56c153d147322c652cce
minho <chaminho88@naver.com>
2020-05-27 22:25:05 +0900

    Make Timer and Game Canvas

---
+++ Assets/Study/Scene/Script/Timer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float LimitTime;
+    public Text text_Timer;
+
+	// Update is called once per frame
+	void Update () {
+        LimitTime += Time.deltaTime;
+        text_Timer.text = "시 간 : " + Mathf.Round(LimitTime);	
+	}
+}

5b862ff75cedaa14e5e2bcc74bf460ec6bc24a41
minho <chaminho88@naver.com>
2020-05-31 20:29:49 +0900

    Make Chasing Code

--- Assets/Study/Scene/Script/Chasing.cs
+++ Assets/Study/Scene/Script/Chasing.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class Chasing : MonoBehaviour {
+    private Transform pursuertr;
+    private Transform playertr;
+    private NavMeshAgent nvAgent;
+
 
 	// Use this for initialization
 	void Start () {
+        pursuertr = gameObject.GetComponent<Transform>();
+        playertr = GameObject.FindWithTag("Player").GetComponent<Transform>();
+
+        nvAgent = gameObject.GetComponent<NavMeshAgent>();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        nvAgent.destination = playertr.position;
+    }
 }

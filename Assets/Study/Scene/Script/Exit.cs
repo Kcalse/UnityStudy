f85c4cb0515af1ab99722b591b87d1aead3c16aa
minho <chaminho88@naver.com>
2020-06-04 23:55:32 +0900

    message
    Signed-off-by: minho <chaminho88@naver.com>

---
+++ Assets/Study/Scene/Script/Exit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+    
+    public void OnClickExit()
+    {
+        Application.Quit();
+        Debug.Log("Button CLick");
+    }
+
+}
